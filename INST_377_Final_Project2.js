8fc22e51a3ae3b7b7dac4f18255420cd0389416d
JasonRahimi2 <159508472+JasonRahimi2@users.noreply.github.com>
2024-05-19 22:16:02 -0400

    Update INST_377_Final_Project2.js

--- INST_377_Final_Project2.js
+++ INST_377_Final_Project2.js
@@ -1,7 +1,7 @@
 async function getMostRecent() {
     
     //const apiURL = 'http://localhost:3000/spotifylyrics'
-    const apiURL = 'https://inst-377-group-final-repo-woad.vercel.app/api/spotifylrics'
+    const apiURL = 'https://inst-377-group-final-repo-woad.vercel.app/api/spotifylyrics'
     await fetch(apiURL)
     .then((response) => response.json())
     .then((data) => {
